95f8ee6b626b0b4d659238129df197512e64e3ac
Ladd1993 <aladd1993@gmail.com>
2022-08-22 12:30:18 -0500

    Refactor

---
+++ src/components/Navigation/LinksArray.jsx
@@ -0,0 +1,30 @@
+export const Links = [
+    {
+        title: "Home",
+        location: "#"
+    },
+    {
+        title: "BŪP Smart Devices",
+        location: "#products"
+    },
+    {
+        title: "Mockups",
+        location: "#mockups"
+    },
+    {
+        title: "Features",
+        location: "#features"
+    },
+    {
+        title: "Pricing",
+        location: "#pricing"
+    },
+    {
+        title: "FAQ",
+        location: "#faq"
+    },
+    {
+        title: "Login",
+        location: "https://www.bup.bio/login"
+    },
+]
